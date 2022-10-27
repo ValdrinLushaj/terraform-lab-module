7a3955a2b981a73e6d73ac44afca7a60931fc3d7
Valdrin Lushaj <valdrinlushaj@hotmail.com>
2022-10-27 20:51:43 +0200

    add user-valdrin-module and variables

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+module "user-valdrin-module" {
+  source  = "app.terraform.io/devops-training-valdrin/user-valdrin-module/iam"
+  version = "1.0.0"
+  # insert required variables here
 
+  user_name   = "valdrin-user"
+  policy_name = "valdrin-policy"
+  policy_document = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*", "iam:GetAccountPasswordPolicy", "elasticloadbalancing:DescribeLoadBalancers"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:ChangePassword"
+        ]
+        Effect   = "Allow"
+        Resource = aws_iam_user.lb.arn
+      }
+    ]
+  }
+}
