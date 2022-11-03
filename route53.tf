3a33ff53bfbb77a1b0cf07206e272d993bb6deb3
Valdrin Lushaj <valdrinlushaj@hotmail.com>
2022-11-03 19:07:51 +0100

    add load balancer

--- route53.tf
+++ route53.tf
@@ -3,6 +3,14 @@ data "aws_route53_zone" "selected" {
   private_zone = false
 }
 
+data "aws_lb" "this" {
+  tags = {
+    "ingress.k8s.aws/resource" = "LoadBalancer",
+    "ingress.k8s.aws/stack"    = "default",
+    "elbv2.k8s.aws/cluster"    = "my-eks-cluster"
+  }
+}
+
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = "web.valdrin.appstellar.training"
