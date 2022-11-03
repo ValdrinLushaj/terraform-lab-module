b4e4045d0185423f0030ebebf3f78521ea72fb87
Valdrin Lushaj <valdrinlushaj@hotmail.com>
2022-11-03 19:32:49 +0100

    update aws route53 resource, add alias

--- route53.tf
+++ route53.tf
@@ -15,6 +15,9 @@ resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = local.subdomain
   type    = "A"
-  ttl     = 300
-  records = [data.aws_lb.this.dns_name]
+  alias {
+    name                   = data.aws_lb.this.dns_name
+    zone_id                = data.aws_route53_zone.selected.zone_id
+    evaluate_target_health = true
+  }
 }
