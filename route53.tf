11ed217876398bf39861e6a5cef29335dfb4f202
Valdrin Lushaj <valdrinlushaj@hotmail.com>
2022-11-03 20:05:03 +0100

    update zone_id to route53

--- route53.tf
+++ route53.tf
@@ -17,7 +17,7 @@ resource "aws_route53_record" "www" {
   type    = "A"
   alias {
     name                   = data.aws_lb.this.dns_name
-    zone_id                = data.aws_route53_zone.selected.zone_id
+    zone_id                = data.aws_lb.this.zone_id
     evaluate_target_health = true
   }
 }
